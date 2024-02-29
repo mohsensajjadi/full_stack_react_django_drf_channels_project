b1d1ae5e8468c626b2f9fdf1afd2593477c679c1
mohsen <mr.mohsensajjadi@gmail.com>
2024-02-29 22:56:51 +0330

    add core app and build custom user model

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.CustomUser'
